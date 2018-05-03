00c2751419ea51ce6137f34877fb8bf06ea0f057
Matt Wilson (XBOX) <matwils@microsoft.com>
2018-05-02 22:43:15 -0700

    Fixing resjson issues.  Removing merge issues.

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
-<<<<<<< HEAD
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import './polyfills.ts';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { CoreEnvironment } from '@msft-sme/shell/core';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-    enableProdMode();
-}
-
-// initialize SME module environment with localization settings.
-CoreEnvironment.initialize(
-    {
-        name: '{!company-name}.{!product-name}',
-        isProduction: environment.production,
-        shellOrigin: '*'
-    },
-    {
-        resourcesPath: 'assets/strings'
-    })
-    .then(() => platformBrowserDynamic().bootstrapModule(AppModule));
-=======
 import './polyfills.ts';
 
 import { enableProdMode } from '@angular/core';
@@ -48,4 +24,3 @@ CoreEnvironment.initialize(
         resourcesPath: 'assets/strings'
     })
     .then(() => platformBrowserDynamic().bootstrapModule(AppModule));
->>>>>>> master
