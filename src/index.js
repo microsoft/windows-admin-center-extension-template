080a6a2dd608230796d05035b14f47d5d9ebcc39
Matt Wilson (XBOX) <matwils@microsoft.com>
2018-07-03 15:22:55 -0700

    Initial pass at the version switch.

--- src/index.js
+++ src/index.js
@@ -18,45 +18,42 @@ const manifestUrlSearch = '{!manifest-url}'
 **/
 const cwd = process.cwd();
 const templatePath = __dirname.substring(0, __dirname.length - 3) + 'templates\\windows-admin-center-extension-template';
-/*
-console.log(templatePath);
-console.log(__dirname);
-console.log(argv.create);
-console.log(process.argv);
-*/
 
-/*
 console.log(templatePath);
 console.log(__dirname);
 console.log(argv.create);
 console.log(process.argv);
-*/
+console.log(argv.version);
 
 if (argv.length === 0) {
-  console.error('Usage: wac create --company <company-name> --tool-name <tool-name> [--verbose]');
-  process.exit(1);
+	console.error('Usage: wac create --company <company-name> --tool-name <tool-name> [--verbose]');
+	process.exit(1);
 }
 
 let normalizedCompany = normalizeString(argv.company);
 let normalizedTool = normalizeString(argv.tool);
 
-create(normalizedCompany, normalizedTool);
+let version = argv.version ? argv.version : '';
 
-function create(company, product) {
-	if(pathExists.sync(product)){
+console.log('version: ' + version);
+
+create(normalizedCompany, normalizedTool, version);
+
+function create(company, product, version) {
+	if (pathExists.sync(product)) {
 		console.error('This tool definition already exists.  No changes have been made.')
 	} else {
 		let productPath = './' + product;
 		fse.mkdirSync(product);
 
 		fse.copySync(templatePath, productPath);
-		updateFiles(productPath, company, product);
+		updateFiles(productPath, company, product, version);
 
 		printOutro(product);
 	}
 }
 
-function updateFiles(path, company, product) {
+function updateFiles(path, company, product, version) {
 	/* 
 	/ files that need updating:
 	/	root package.json
@@ -69,35 +66,55 @@ function updateFiles(path, company, product) {
 	let manifestFilePath = './' + product + '/src/manifest.json';
 	let mainFilePath = './' + product + '/src/main.ts';
 	let stringsFilePath = './' + product + '/src/resources/strings/strings.resjson';
-	
+
 	let packageName = '@' + company + '/' + product;
 	let manfiestName = company.toLowerCase() + '.' + product.toLowerCase();
 	let stringsProduct = product.split('-').join(''); // Strings file cannot handle dashes.
-	
-	cleanDirectory[rootPackagePath] = { '@{!company-name}/{!product-name}': packageName };
-	
-	cleanDirectory[manifestFilePath] = { '{!company-name}.{!module-name}': manfiestName, 
+
+	if (version) {
+		let littleVersion = version.toLowerCase();
+		if (littleVersion === 'next' || littleVersion === 'insider') {
+			let existingVersion = '"@microsoft/windows-admin-center-sdk": "latest",';
+			cleanDirectory[rootPackagePath] = {
+				'@{!company-name}/{!product-name}': packageName,
+				'"@microsoft/windows-admin-center-sdk": "latest",': existingVersion.replace('latest', littleVersion)
+			};
+		}
+		else {
+			cleanDirectory[rootPackagePath] = { '@{!company-name}/{!product-name}': packageName };
+		}
+	}
+	else {
+		cleanDirectory[rootPackagePath] = { '@{!company-name}/{!product-name}': packageName };
+	}
+
+	cleanDirectory[manifestFilePath] = {
+		'{!company-name}.{!module-name}': manfiestName,
 		'{!company-name}.{!product-name}': manfiestName,
 		'{!product-display-name}': product,
-		'{blank}': product.toLowerCase() };
+		'{blank}': product.toLowerCase()
+	};
 
-	cleanDirectory[stringsFilePath] = { '{!product-display-name}': stringsProduct,
+	cleanDirectory[stringsFilePath] = {
+		'{!product-display-name}': stringsProduct,
 		'{!product-title}': stringsProduct,
-		'{!ProductName}': stringsProduct};
+		'{!ProductName}': stringsProduct
+	};
 
 	cleanDirectory[mainFilePath] = { '{!company-name}.{!product-name}': manfiestName };
 
-	for(var key in cleanDirectory) {
+	for (var key in cleanDirectory) {
 		cleanFile(key, cleanDirectory[key]);
 	}
 }
 
 function cleanFile(key, values) {
 	console.log('Updating: ' + key);
-	for(var valuesKey in values) {
+	for (var valuesKey in values) {
+		console.log('Looking for:' + valuesKey + ' - ' + values[valuesKey]);
 		let fileData = fse.readFileSync(key, 'utf8');
 		let displayNameIndex = fileData.indexOf(valuesKey);
-		while(displayNameIndex > 0) {
+		while (displayNameIndex > 0) {
 			fileData = fileData.replace(valuesKey, values[valuesKey]);
 			displayNameIndex = fileData.indexOf(valuesKey);
 		}
